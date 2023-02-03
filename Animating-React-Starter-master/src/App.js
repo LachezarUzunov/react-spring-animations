1690ea1463977ace36d4ebdff21064298e059483
lachezaruzunov <uzunov_89_lacho@abv.bg>
2023-02-03 10:49:58 +0200

    Animating Navigation Menu

--- Animating-React-Starter-master/src/App.js
+++ Animating-React-Starter-master/src/App.js
@@ -1,18 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import { useSpring, animated } from "react-spring";
 import Toggle from "./Toggle";
 import logo from "./logo.svg";
 import "./App.css";
+import Nav from "./Nav";
 
 const App = () => {
+  const [isNavOpen, setNavOpen] = useState(false);
+
   const fade = useSpring({ from: { opacity: 0 }, opacity: 1 });
+
+  const navAnimation = useSpring({
+    transform: isNavOpen ? `translate3d(0, 0, 0)` : `translate3d(100%, 0, 0)`,
+  });
 
   return (
     <animated.div className="App" style={fade}>
       <header className="App-header">
         <img src={logo} className="logo" alt="logo" />
-        <button className="menu-button">Menu</button>
+        <button onClick={() => setNavOpen(!isNavOpen)} className="menu-button">
+          Menu
+        </button>
       </header>
+      <Nav style={navAnimation} />
       <main>
         <Toggle />
       </main>
