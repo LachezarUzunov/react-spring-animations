3c402efa86b3dba092f6ccebac8b7dd5617f340d
lachezaruzunov <uzunov_89_lacho@abv.bg>
2023-02-03 09:28:32 +0200

    adding interpolation

--- Animating-React-Starter-master/src/Toggle.js
+++ Animating-React-Starter-master/src/Toggle.js
@@ -1,17 +1,31 @@
 import React, { useState } from "react";
 import { useSpring, animated } from "react-spring";
-import AnimatedTitle from "./AnimatedTitle";
+//import AnimatedTitle from "./AnimatedTitle";
 
 const Toggle = () => {
   const [toggled, setIsToggled] = useState(false);
-  const fade = useSpring({
+  //   const fade = useSpring({
+  //     opacity: toggled ? 1 : 0.5,
+  //     fontSize: toggled ? "2rem" : "4rem",
+  //     y: toggled ? 0 : -50,
+  //   });
+
+  const { opacity, fontSize, y } = useSpring({
     opacity: toggled ? 1 : 0.5,
     fontSize: toggled ? "2rem" : "4rem",
+    y: toggled ? 0 : -50,
   });
 
   return (
     <div>
-      <AnimatedTitle style={fade}>Hello</AnimatedTitle>
+      <animated.h1
+        style={{
+          transform: y.interpolate((y) => `translate3d(0, ${y}px, 0)`),
+          opacity,
+        }}
+      >
+        Hello
+      </animated.h1>
       <button onClick={() => setIsToggled(!toggled)}>Toggle</button>
     </div>
   );
